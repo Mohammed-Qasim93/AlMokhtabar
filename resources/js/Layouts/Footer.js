eb907d58372ed66e1edaf0ca0b8b07a40c4383bf
unknown <professor404@gmail.com>
2022-01-21 23:03:21 +0300

    Ff

--- resources/js/Layouts/Footer.js
+++ resources/js/Layouts/Footer.js
@@ -2,20 +2,20 @@ import React from "react";
 import ApplicationLogo from "@/Components/ApplicationLogo";
 import { Link } from "@inertiajs/inertia-react";
 
-export default function Footer({}) {
+export default function Footer({ }) {
     return (
         <div className="h-64 flex  flex-col sm:justify-center items-center pt-6  sm:pt-0 bg-gray-100">
             <div className="w-full h-full flex flex-col justify-between px-6 py-4 bg-white shadow-md mt-16 overflow-hidden sm:rounded-lg">
                 <div className="flex p-4 items-center justify-around">
                     <div className="flex flex-col justify-start">
-                        <ApplicationLogo className="w-[180px] h-[50px]" />
+                        <ApplicationLogo className="mx-auto w-[180px] h-[50px]" />
                         <div className="text-center">
                             <p>Love your self</p>
-                            <p>All rights reserved &copy;</p>
+                            <p>All rights reserved &copy; to Almokhtabar Labs {(new Date().getFullYear())}</p>
                         </div>
                     </div>
                     <div className="flex flex-col gap-4 justify-center capitalize">
-                        <p>tech support</p>
+                        <p>HorizonDev tech support</p>
                         <div className="flex items-center justify-center gap-4">
                             <a href="https://www.instagram.com/horizondev.iq/">
                                 <svg
@@ -42,10 +42,10 @@ export default function Footer({}) {
                 </div>
                 <div className=" border-t-2 w-full bottom-0">
                     <div className="flex p-4 justify-center items-center gap-x-1">
-                        <p>Developed by </p>
-                        <a href="https://horizondev-7ul7z3h2p-mohammed-qasim93.vercel.app/">
+                        <p>Programmed & Developed by </p>
+                        <a className="text-blue-500 underline" target="_blank" href="https://horizondev-7ul7z3h2p-mohammed-qasim93.vercel.app/">
                             {" "}
-                            Horizon Dev{" "}
+                            HorizonDev{" "}
                         </a>
                     </div>
                 </div>
