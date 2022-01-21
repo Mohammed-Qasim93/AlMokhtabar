9353e95cb245bb8cea2eb7752b975500a5662c12
unknown <professor404@gmail.com>
2022-01-22 00:43:13 +0300

    Ff

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -47,11 +47,11 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        event(new Registered($user));
+        // event(new Registered($user));
 
-        Auth::login($user);
+        // Auth::login($user);
 
         // return redirect(RouteServiceProvider::HOME);
-        return Redirect::route('index');
+        return Redirect::route('user.index');
     }
 }
